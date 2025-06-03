b6d6dfbad6e787d50025406d16826b2240eb0f34
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-03 17:28:13 +0100

    remove extras

--- imports/modules/hosts/gouda/hardware.nix
+++ imports/modules/hosts/gouda/hardware.nix
@@ -1,9 +1,14 @@
-{ inputs, modulesPath, lib, ... }:
 {
+  inputs,
+  modulesPath,
+  lib,
+  ...
+}:
+{
+  imports = [ "${modulesPath}/profiles/perlless.nix" ];
   unify.hosts.nixos.gouda.nixos = {
     imports = [
-      {system.forbiddenDependenciesRegexes = lib.mkForce [ ];}
-      "${modulesPath}/profiles/perlless.nix"
+      { system.forbiddenDependenciesRegexes = lib.mkForce [ ]; }
       inputs.nixos-hardware.nixosModules.common-pc-ssd
     ];
   };
