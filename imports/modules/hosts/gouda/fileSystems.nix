2c548f595a9542c4f6f2967bd38d87cf54b4457a
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-03 18:27:53 +0100

    fix some things

--- imports/modules/hosts/gouda/fileSystems.nix
+++ imports/modules/hosts/gouda/fileSystems.nix
@@ -1,106 +1,106 @@
 {
-  config,
   lib,
   pkgs,
-  modulesPath,
   ...
 }:
 {
-  unify.hosts.nixos.gouda.nixos = {
-    fileSystems."/" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [
-        "rw"
-        "relatime"
-        "compress=zstd:3"
-        "ssd"
-        "discard=async"
-        "space_cache=v2"
-        "subvol=/@"
-      ];
+  unify.hosts.nixos.gouda.nixos =
+    { config, ... }:
+    {
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [
+          "rw"
+          "relatime"
+          "compress=zstd:3"
+          "ssd"
+          "discard=async"
+          "space_cache=v2"
+          "subvol=/@"
+        ];
+      };
+
+      fileSystems."/.snapshots" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [
+          "rw"
+          "relatime"
+          "compress=zstd:3"
+          "ssd"
+          "discard=async"
+          "space_cache=v2"
+          "subvol=/@.snapshots"
+        ];
+      };
+
+      fileSystems."/nix" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [
+          "rw"
+          "relatime"
+          "compress=zstd:3"
+          "ssd"
+          "discard=async"
+          "space_cache=v2"
+          "subvol=/@nix"
+        ];
+      };
+
+      fileSystems."/home" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [
+          "rw"
+          "relatime"
+          "compress=zstd:3"
+          "ssd"
+          "discard=async"
+          "space_cache=v2"
+          "subvol=/@home"
+        ];
+      };
+
+      fileSystems."/var/log" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [
+          "rw"
+          "relatime"
+          "compress=zstd:3"
+          "ssd"
+          "discard=async"
+          "space_cache=v2"
+          "subvol=/@log"
+        ];
+      };
+
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/4EB7-2C5E";
+        fsType = "vfat";
+        options = [
+          "rw"
+          "relatime"
+          "fmask=0022"
+          "dmask=0022"
+          "codepage=437"
+          "iocharset=ascii"
+          "shortname=mixed"
+          "utf8"
+          "errors=remount-ro"
+        ];
+      };
+
+      fileSystems."/swap" = {
+        device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
+        fsType = "btrfs";
+        options = [ "subvol=/@swap" ];
+      };
+      swapDevices = [ { device = "/swap/swapfile"; } ];
+
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
     };
-
-    fileSystems."/.snapshots" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [
-        "rw"
-        "relatime"
-        "compress=zstd:3"
-        "ssd"
-        "discard=async"
-        "space_cache=v2"
-        "subvol=/@.snapshots"
-      ];
-    };
-
-    fileSystems."/nix" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [
-        "rw"
-        "relatime"
-        "compress=zstd:3"
-        "ssd"
-        "discard=async"
-        "space_cache=v2"
-        "subvol=/@nix"
-      ];
-    };
-
-    fileSystems."/home" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [
-        "rw"
-        "relatime"
-        "compress=zstd:3"
-        "ssd"
-        "discard=async"
-        "space_cache=v2"
-        "subvol=/@home"
-      ];
-    };
-
-    fileSystems."/var/log" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [
-        "rw"
-        "relatime"
-        "compress=zstd:3"
-        "ssd"
-        "discard=async"
-        "space_cache=v2"
-        "subvol=/@log"
-      ];
-    };
-
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/4EB7-2C5E";
-      fsType = "vfat";
-      options = [
-        "rw"
-        "relatime"
-        "fmask=0022"
-        "dmask=0022"
-        "codepage=437"
-        "iocharset=ascii"
-        "shortname=mixed"
-        "utf8"
-        "errors=remount-ro"
-      ];
-    };
-
-    fileSystems."/swap" = {
-      device = "/dev/disk/by-uuid/279093a1-315d-4896-bd15-dac40458bc15";
-      fsType = "btrfs";
-      options = [ "subvol=/@swap" ];
-    };
-    swapDevices = [ { device = "/swap/swapfile"; } ];
-
-    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  };
 }
