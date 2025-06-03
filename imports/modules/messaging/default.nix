c375b182e51f7901c9763a905aa43a82ef13f52a
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-03 17:36:53 +0100

    fix some things

--- imports/modules/messaging/default.nix
+++ imports/modules/messaging/default.nix
@@ -1,21 +1,19 @@
 {
-  inputs,
-  ...
-}:
-{
-  unify.modules.messaging.home = { pkgs, ... }: {
-    nixpkgs.overlays = [
-      inputs.self.overlays.default
-    ];
+  unify.modules.messaging.home =
+    { pkgs, inputs, ... }:
+    {
+      nixpkgs.overlays = [
+        inputs.self.overlays.default
+      ];
 
-    home.packages = [
-      pkgs.local.chromium-discord
-      pkgs.local.chromium-element
-      pkgs.local.chromium-meet
-      pkgs.local.chromium-protonmail
-      pkgs.local.chromium-teams
-      pkgs.local.chromium-telegram
-      pkgs.signal-desktop
-    ];
-  };
+      home.packages = [
+        pkgs.local.chromium-discord
+        pkgs.local.chromium-element
+        pkgs.local.chromium-meet
+        pkgs.local.chromium-protonmail
+        pkgs.local.chromium-teams
+        pkgs.local.chromium-telegram
+        pkgs.signal-desktop
+      ];
+    };
 }
