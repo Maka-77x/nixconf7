dbf8058b2ea9358fa6cb6a0e43a58d4d90992326
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-08 15:42:50 +0100

    some bits

--- modules/bluetooth/default.nix
+++ modules/bluetooth/default.nix
@@ -1,9 +1,10 @@
 {
   flake.modules.nixos.bluetooth =
-  {pkgs, ...}:{
-    hardware.bluetooth.enable = true;
-    hardware.bluetooth.package = pkgs.bluez;
-    hardware.bluetooth.powerOnBoot = false;
-    hardware.bluetooth.settings.General.Experimental = true;
-  };
+    { pkgs, ... }:
+    {
+      hardware.bluetooth.enable = true;
+      hardware.bluetooth.package = pkgs.bluez-experimental;
+      hardware.bluetooth.powerOnBoot = false;
+      hardware.bluetooth.settings.General.Experimental = true;
+    };
 }
