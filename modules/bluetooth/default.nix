4445785b8ff93fadc54fc26c993aad92c2bd4e17
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-08 15:34:20 +0100

    hardware - bluetooth

--- modules/bluetooth/default.nix
+++ modules/bluetooth/default.nix
@@ -1,5 +1,6 @@
-{pkgs, ...}:{
-  flake.modules.nixos.bluetooth = {
+{
+  flake.modules.nixos.bluetooth =
+  {pkgs, ...}:{
     hardware.bluetooth.enable = true;
     hardware.bluetooth.package = pkgs.bluez;
     hardware.bluetooth.powerOnBoot = false;
