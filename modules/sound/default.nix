d461a73f383e8dc31d85552f3ae5c91892eeee41
Michael Parrett <99078878+Maka-77x@users.noreply.github.com>
2025-06-11 18:31:30 +0100

    some more things

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -7,20 +7,20 @@
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = false;
-        extraConfig = {
-          #     "99-input-denoising" = rnnoise_config;
-          adjust-sample-rate."context.properties"."default.clock.rate" = 48000;
-          adjust-sample-rate."context.properties"."default.allowed-rates" = [
-            192000
-            96000
-            48000
-            44100
-          ];
-          #     adjust-sample-rate."context.properties"."default.allowed-rates" = [ 192000 ];
-          #     adjust-sample-rate."context.properties"."default.clock.quantum" = 32;
-          #     adjust-sample-rate."context.properties"."default.clock.min-quantum" = 32;
-          #     adjust-sample-rate."context.properties"."default.clock.max-quantum" = 32;
-        };
+        # extraConfig = {
+        #   #     "99-input-denoising" = rnnoise_config;
+        #   adjust-sample-rate."context.properties"."default.clock.rate" = 48000;
+        #   adjust-sample-rate."context.properties"."default.allowed-rates" = [
+        #     192000
+        #     96000
+        #     48000
+        #     44100
+        #   ];
+        #     adjust-sample-rate."context.properties"."default.allowed-rates" = [ 192000 ];
+        #     adjust-sample-rate."context.properties"."default.clock.quantum" = 32;
+        #     adjust-sample-rate."context.properties"."default.clock.min-quantum" = 32;
+        #     adjust-sample-rate."context.properties"."default.clock.max-quantum" = 32;
+        # };
         wireplumber.enable = true;
       };
     };
